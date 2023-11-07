ce61d2d592ed9e8f20f460fbfa3d6474d49fbf4f
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-07 13:46:08 +0200

    exercise 6.1 done

--- unicafe-redux/.eslintrc.cjs
+++ unicafe-redux/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, "jest/globals": true },
+  env: { browser: true, es2020: true, 'jest/globals': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -10,11 +10,41 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jest'],
   rules: {
+    indent: [
+      'error',
+      4,
+      { 'SwitchCase':  1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'arrow-spacing': [
+      'error',
+      { before: true, after: true }
+    ],
+    'no-console': 0,
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 0
   },
 }
