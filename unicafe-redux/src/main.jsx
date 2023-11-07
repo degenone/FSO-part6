6b82951c407cc5e3be296a5c8e13ea464a214898
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-07 14:10:39 +0200

    exercise 6.2 done

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -6,21 +6,50 @@ import reducer from './reducer';
 const store = createStore(reducer);
 
 const App = () => {
-    const good = () => {
-        store.dispatch({
-            type: 'GOOD',
-        });
+    const clickHandler = (action) => () => store.dispatch({ type: action });
+    const style = {
+        width: '20rem',
+        padding: 5,
+        marginInline: 'auto',
+        marginBlockStart: '3rem',
+        border: '1px solid black',
+        display: 'grid',
+        gap: '0.5rem',
+        gridTemplateAreas: `
+            'btn-g btn-o btn-b btn-z'
+            'g . . .'
+            'o . . .'
+            'b . . .'
+        `,
     };
 
     return (
-        <div>
-            <button onClick={good}>good</button>
-            <button>ok</button>
-            <button>bad</button>
-            <button>reset stats</button>
-            <div>good {store.getState().good}</div>
-            <div>ok</div>
-            <div>bad</div>
+        <div style={style}>
+            <button
+                style={{ gridArea: 'btn-g' }}
+                onClick={clickHandler('GOOD')}>
+                good
+            </button>
+            <button style={{ gridArea: 'btn-o' }} onClick={clickHandler('OK')}>
+                ok
+            </button>
+            <button style={{ gridArea: 'btn-b' }} onClick={clickHandler('BAD')}>
+                bad
+            </button>
+            <button
+                style={{ gridArea: 'btn-z' }}
+                onClick={clickHandler('ZERO')}>
+                reset stats
+            </button>
+            <div style={{ gridArea: 'g' }}>
+                good &mdash; {store.getState().good}
+            </div>
+            <div style={{ gridArea: 'o' }}>
+                ok &mdash; {store.getState().ok}
+            </div>
+            <div style={{ gridArea: 'b' }}>
+                bad &mdash; {store.getState().bad}
+            </div>
         </div>
     );
 };
