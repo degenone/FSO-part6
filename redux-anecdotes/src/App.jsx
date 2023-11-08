7153afaaa8cc245f9c41fb38e69d403977a0a72b
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-08 14:01:36 +0200

    exercise 6.5 done

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -3,7 +3,16 @@ import { createNew, voteFor } from './reducers/anecdoteReducer';
 import { useState } from 'react';
 
 const App = () => {
-    const anecdotes = useSelector((state) => state);
+    const anecdotes = useSelector((state) =>
+        state.sort((a, b) => {
+            if (a.votes < b.votes) {
+                return 1;
+            } else if (a.votes > b.votes) {
+                return -1;
+            }
+            return 0;
+        })
+    );
     const dispatch = useDispatch();
     const [anecdote, setAnecdote] = useState('');
 
